3e368a0c4b0e5a499175ba737cc92d5f77bf4f90
Idalia Z. Espino Lira <idalia.espino12@gmail.com>
2022-08-01 00:11:17 -0500

    done

---
+++ src/main/java/com/techtalentsouth/TechTalentBlog/TechTalanetBlogApplication.java
@@ -0,0 +1,13 @@
+package com.techtalentsouth.TechTalentBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTalanetBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechTalanetBlogApplication.class, args);
+	}
+
+}
